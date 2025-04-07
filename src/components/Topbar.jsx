37df2eaf9f0c25a48a9acf2b018a8de40c484191
Rinkoo-ctrl <rinkookandpal2000@gmail.com>
2025-04-07 22:37:11 +0530

    Modify topbar for profile picture

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -34,7 +34,7 @@ const Topbar = () => {
                     <img
                         src={imageUrl}
                         alt="Profile"
-                        className="w-10 h-10 rounded-full object-cover border-2 border-purple-400 shadow-md transition-all duration-300 hover:border-purple-600"
+                        className="w-10 h-10 rounded-full object-cover shadow-md transition-all duration-300"
                     />
                     <svg xmlns="http://www.w3.org/2000/svg" className={`h-5 w-5 transition-transform duration-300 ${dropdownOpen ? 'transform rotate-180' : ''}`} viewBox="0 0 20 20" fill="currentColor">
                         <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
